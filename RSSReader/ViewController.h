469530a8bbb9a16122384bf33eff4a7e3942fe4a
Vasiliev Oleg <stalikoff@gmail.com>
2015-12-14 23:26:00 +0200

    Initial Commit

---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by Oleg on 14.12.15.
+//  Copyright © 2015 Oleg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
